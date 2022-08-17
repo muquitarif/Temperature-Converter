46d66fea1038655e829a1488fa446a910e403b10
Muquit Arif <muquitarif2@gmail.com>
2022-08-18 02:06:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let dark = document.getElementById("dark");
+let light = document.getElementById("light");
+let celsius = document.getElementById("Celsius");
+let farenheit = document.getElementById("Fahrenheit");
+
+// THEME BUTTONS
+dark.addEventListener("click", function () {
+    document.body.style.backgroundColor = "#202020";
+    document.getElementById("h1").style.color = "#FFFFFF";
+    document.getElementById("p").style.color = "#FFFFFF";
+    document.getElementById("theme").style.color = "#FFFFFF";
+});
+
+light.addEventListener("click", function () {
+    document.body.style.backgroundColor = "#FFFFFF";
+    document.getElementById("h1").style.color = "#202020";
+    document.getElementById("p").style.color = "#202020";
+    document.getElementById("theme").style.color = "#202020";
+});
+
+// TEMPERATURE CONVERTER
+celsius.addEventListener("input", function () {
+    let tempCelsius = this.value;
+    farenheit.value = tempCelsius * 9 / 5 + 32;
+});
+
+farenheit.addEventListener("input", function () {
+    let tempFarenheit = this.value;
+    celsius.value = (tempFarenheit - 32) * 5 / 9;
+});
