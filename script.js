29e0c64cefc79959b3d3b21ef1edb93b6e23f280
muhammadumar000 <FA20-BEE-150@ISBSTUDENT.COMSATS.EDU.PK>
2022-08-18 21:16:13 +0500

    added if conditions in eventlisteners

--- script.js
+++ script.js
@@ -5,28 +5,37 @@ let farenheit = document.getElementById("Fahrenheit");
 
 // THEME BUTTONS
 dark.addEventListener("click", function () {
-    document.body.style.backgroundColor = "#202020";
-    document.getElementById("h1").style.color = "#FFFFFF";
-    document.getElementById("p").style.color = "#FFFFFF";
-    document.getElementById("theme").style.color = "#FFFFFF";
+  document.body.style.backgroundColor = "#202020";
+  document.getElementById("h1").style.color = "#FFFFFF";
+  document.getElementById("p").style.color = "#FFFFFF";
+  document.getElementById("theme").style.color = "#FFFFFF";
 });
 
 light.addEventListener("click", function () {
-    document.body.style.backgroundColor = "#FFFFFF";
-    document.getElementById("h1").style.color = "#202020";
-    document.getElementById("p").style.color = "#202020";
-    document.getElementById("theme").style.color = "#202020";
+  document.body.style.backgroundColor = "#FFFFFF";
+  document.getElementById("h1").style.color = "#202020";
+  document.getElementById("p").style.color = "#202020";
+  document.getElementById("theme").style.color = "#202020";
 });
 
 // TEMPERATURE CONVERTER
+
 celsius.addEventListener("input", function () {
-    let tempCelsius = this.value;
-    let result = tempCelsius * 9 / 5 + 32;
-    farenheit.value = `${result}° F`
+  let tempCelsius = this.value;
+  if (tempCelsius) {
+    let result = (tempCelsius * 9) / 5 + 32;
+    farenheit.value = `${result}° F`;
+  } else {
+    farenheit.value = null;
+  }
 });
 
 farenheit.addEventListener("input", function () {
-    let tempFarenheit = this.value;
-    let result = (tempFarenheit - 32) * 5 / 9;
+  let tempFarenheit = this.value;
+  if (tempFarenheit) {
+    let result = ((tempFarenheit - 32) * 5) / 9;
     celsius.value = `${result}° C`;
+  } else {
+    celsius.value = null;
+  }
 });
